2e117ffd45ca3d97f620a26ed4562a575ddd144d
Liam11063 <99927032+Liam11063@users.noreply.github.com>
2023-06-01 11:32:36 +1000

    fixed listener issue in generator

--- P2Generator.js
+++ P2Generator.js
@@ -1,7 +1,18 @@
 const quotes = [];
 const random = [];
 
-quotes.push("quote 1", "quote 2", "quote 3", "quote 4", "quote 5");
+quotes.push(
+  "quote 1",
+  "quote 2",
+  "quote 3",
+  "quote 4",
+  "quote 5",
+  "quote 6",
+  "quote 7",
+  "quote 8",
+  "quote 9",
+  "quote 10"
+);
 random.push("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
 
 const button = document.getElementById("generate");
@@ -14,7 +25,12 @@ function inputAction() {
 }
 
 function onclick() {
-    msg.textContent = generatedQuote
+  if (msg.textContent == generatedQuote) {
+    msg.textContent =
+      quotes[Math.floor(Math.random() * Math.random(random) * quotes.length)];
+  } else {
+    msg.textContent = generatedQuote;
+  }
 }
 
-button.onclick = onclick
+button.onclick = onclick;
