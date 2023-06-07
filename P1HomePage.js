1560b0647faf702cc4628a99683a68b3207d9b74
Liam11063 <99927032+Liam11063@users.noreply.github.com>
2023-06-07 12:34:51 +1000

    Update P1HomePage.js

---
+++ P1HomePage.js
@@ -0,0 +1,62 @@
+const button = document.getElementById("pageLinkTwo");
+const navigationMenuDiv = document.getElementById("navigationMenuOptions");
+const navigationButton = document.getElementById("navigationMenu");
+
+if (!button || !navigationMenuDiv || !navigationButton)
+  alert(
+    "An error has occured while trying to operate this page. Please reload this site!"
+  );
+
+function navBar() {
+  if (
+    window.getComputedStyle(navigationMenuDiv).backgroundColor ==
+    "rgb(1, 37, 122)"
+  ) {
+    navigationMenuDiv.style.visibility = "visible";
+    document.body.style.backgroundColor = "#d9d9d9";
+    navigationMenuDiv.style.backgroundColor = "#C2C1C1";
+    navigationMenuDiv.style.marginLeft = "5px";
+    navigationMenuDiv.style.marginTop = "0px";
+    navigationMenuDiv.style.height = "max-content";
+  } else {
+    location.reload() // Using navigationMenuDiv.style.visibility = "hidden" would cause a error when you attempt to open the navigation menu again
+  }
+}
+
+navigationButton.onclick = navBar;
+
+function generateQuote() {
+  window.location.href = "./P2Generator.html";
+}
+
+button.onclick = generateQuote;
+
+const homeMenu = document.getElementById("navigationButtons_Home");
+const generatorMenu = document.getElementById("navigationButtons_Generator");
+const bookmarkMenu = document.getElementById("navigationButtons_Bookmarks");
+const discoverMenu = document.getElementById("navigationButtons_Discover");
+
+function homePage() {
+  window.location.href = "./index.html";
+  homeMenu.disabled = true
+}
+
+homeMenu.onclick = homePage;
+
+function generatorPage() {
+  window.location.href = "./P2Generator.html";
+}
+
+generatorMenu.onclick = generatorPage;
+
+function bookmarkPage() {
+  window.location.href = "./P3Bookmark.html";
+}
+
+bookmarkMenu.onclick = bookmarkPage;
+
+function discoverPage() {
+  window.location.href = "./P4Discover.html";
+}
+
+discoverMenu.onclick = discoverPage;
