e0064b229a2ec0ea7b411f141b94377926cd5891
Liam11063 <99927032+Liam11063@users.noreply.github.com>
2023-06-07 12:32:58 +1000

    changes to the navigation bar

--- P4Discover.js
+++
@@ -1,4 +0,0 @@
-// code to push to github
-// git add "file name"
-// git commit -m "commit reason"
-// git push
---
+++ gitPush.js
@@ -0,0 +1,5 @@
+// git add * or git add <fileName>
+  // * = all files, <fileName> is individual files
+// git commit -m "reason for commit"
+// git push (push changes to github)
+// git status (shows file status)
